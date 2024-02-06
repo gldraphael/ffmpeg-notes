45e2e5ee03d97db8f920191128794bc26fa541e3
Galdin Raphael <gldraphael@users.noreply.github.com>
2024-02-07 02:01:02 +0530

    Setup GA4 (#3)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-X75PX3CW1E',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
